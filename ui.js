e8fb65b26d52d57c0f73de62e885dba715e074cc
sorja <miro.sorja@gmail.com>
2015-08-17 18:42:55 +0300

    Added own file for ui

---
+++ ui.js
@@ -0,0 +1,20 @@
+
+/*
+  This stuff is outside of the algorithm,
+  it's for the ui
+*/
+
+// Toggle drawing with shift, cause
+// too lazy to move mouse
+window.onkeyup = function(e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+  var checkbox = document.getElementById("draw_on_map");
+  if (key == 16) {
+    checkbox.checked = !checkbox.checked;
+  }
+}
+
+function handleWallChanges(e){
+  chanceOfWall = +e.value/100;
+  createMap();
+}
